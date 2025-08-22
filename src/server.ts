b475c5194d5c6bbd0a557cc8d94bb05ae6cc0d38
Shiva Pasham <shivaprasad.pasham@gmail.com>
2025-08-22 05:15:06 +0100

    add dist

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ export class SeleniumMcpServer {
   constructor() {
     this.server = new McpServer({
       name: "selenium-webdriver-mcp",
-      version: "0.1.2"
+      version: "0.1.3"
     });
 
     this.stateManager = new StateManager();

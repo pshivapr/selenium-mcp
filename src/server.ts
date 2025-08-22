6992f57cf48421c5457528d9608c184c666a2688
Shiva Pasham <shivaprasad.pasham@gmail.com>
2025-08-22 14:56:51 +0100

    update readme

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ export class SeleniumMcpServer {
   constructor() {
     this.server = new McpServer({
       name: "selenium-webdriver-mcp",
-      version: "0.1.5"
+      version: "0.1.6"
     });
 
     this.stateManager = new StateManager();
